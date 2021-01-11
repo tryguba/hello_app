7512e920a392a7b3928b3e81fa04427e908dc9f9
tryguba <trigyba.oleg@gmail.com>
2021-01-11 17:38:07 +0200

    five_1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         You are the best)) ---- Learn React
         </a>
       </header>
     </div>
